2f1484af8342e1db56e00a30d7bbb89bae8eba79
Bradyp34 <brady.petterson@gmail.com>
2024-05-06 11:07:23 -0500

    Add project files.

---
+++ Brady's Conversion Program/Form1.cs
@@ -0,0 +1,10 @@
+namespace Brady_s_Conversion_Program
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Brady's Conversion Program/Functions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brady_s_Conversion_Program
+{
+    internal class Functions
+    {
+        public static void testSetupFunction()
+        {
+            return;
+        }
+    }
+}
