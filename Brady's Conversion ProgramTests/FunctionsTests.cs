5702df1e740945ae6dab689417219c79631e5224
Bradyp34 <brady.petterson@gmail.com>
2024-05-06 15:48:48 -0500

    deleted tests

--- Brady's Conversion ProgramTests/FunctionsTests.cs
+++ Brady's Conversion ProgramTests/FunctionsTests.cs
@@ -16,7 +16,7 @@ namespace Brady_s_Conversion_Program.Tests {
             File.WriteAllText(path, "This is a test file."); // Write to the file
             string result = Functions.UploadFile(path);
             Assert.AreEqual("UploadedFiles/test.txt", result);
-            File.Delete("UploadedFiles/test.txt"); // Clean up the file
+            File.Delete("../../../../UploadedFiles/test.txt"); // Clean up the file
             File.Delete(path); // Clean up the file
         }
     }
