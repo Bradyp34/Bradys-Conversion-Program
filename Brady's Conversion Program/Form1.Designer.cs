0703131f50b15d06051831f25d5be99a1db118ea
Bradyp34 <brady.petterson@gmail.com>
2024-05-06 12:51:34 -0500

    created very base for file. no functionality yet

--- Brady's Conversion Program/Form1.Designer.cs
+++ Brady's Conversion Program/Form1.Designer.cs
@@ -26,14 +26,85 @@
         ///  Required method for Designer support - do not modify
         ///  the contents of this method with the code editor.
         /// </summary>
-        private void InitializeComponent()
-        {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+        private void InitializeComponent() {
+            ServerTextBox = new TextBox();
+            DatabaseTextBox = new TextBox();
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // ServerTextBox
+            // 
+            ServerTextBox.Location = new Point(6, 6);
+            ServerTextBox.Name = "ServerTextBox";
+            ServerTextBox.PlaceholderText = "Server Name";
+            ServerTextBox.Size = new Size(132, 23);
+            ServerTextBox.TabIndex = 0;
+            // 
+            // DatabaseTextBox
+            // 
+            DatabaseTextBox.Location = new Point(6, 35);
+            DatabaseTextBox.Name = "DatabaseTextBox";
+            DatabaseTextBox.PlaceholderText = "Database";
+            DatabaseTextBox.Size = new Size(132, 23);
+            DatabaseTextBox.TabIndex = 1;
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(2, 1);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(797, 447);
+            tabControl1.TabIndex = 2;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(ServerTextBox);
+            tabPage1.Controls.Add(DatabaseTextBox);
+            tabPage1.Location = new Point(4, 24);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(789, 419);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "tabPage1";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 24);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(192, 72);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "tabPage2";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Ivory;
+            ClientSize = new Size(800, 450);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            tabPage1.PerformLayout();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private TextBox ServerTextBox;
+        private TextBox DatabaseTextBox;
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private TabPage tabPage2;
     }
 }
